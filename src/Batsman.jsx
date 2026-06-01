a821818da1dd1acef5b5f1f5a1008ea6cb125c57
Mansur550 <mansurislam59@gmail.com>
2026-06-02 00:35:24 +0600

    Update runns Using state

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -4,10 +4,29 @@ export default function Batsman() {
 
     const [runs, setRuns] = useState(0)
 
+    const handleSingle = () => {
+        const newRuns = runs + 1;
+        setRuns(newRuns);
+    }
+
+    const handleFour = () => {
+        const newRuns = runs + 4;
+        setRuns(newRuns);
+    }
+
+    const handleSix = () => {
+        const newRuns = runs + 6;
+        setRuns(newRuns);
+    }
+
     return (
         <div>
             <h3>Player: Bangladesh</h3>
             <h1>Score: {runs}</h1>
+            <button onClick={handleSingle}>Single</button>
+            <button onClick={handleFour}>Four</button>
+            <button onClick={handleSix}>Six</button>
+
         </div>
     )
 }
